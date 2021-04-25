ddb417ab4ba1fe02e6c43c8c41eee78d674a1f21
deguchi <deguchi.k.110@gmail.com>
2021-04-25 12:42:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 概要
+TerraformでAWSのEC2インスタンスを作成する
+
+https://qiita.com/kazato110tm/items/59d4a780d2262ab96aaa
---
+++ variables.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  version = "~> 2.0"
+  region = "ap-northeast-1"
+  shared_credentials_file = "/Users/`user_name`/.aws/`key_file`"
+}
